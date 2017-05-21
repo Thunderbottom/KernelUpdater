5af8139a376dd60989b90442dd4d2d9e95348108
Simao Gomes Viana <xdevs23@outlook.com>
2017-05-21 10:32:32 +0200

    Move the AsyncTask as well

--- app/src/main/java/io/arsenic/updater/utils/UpdateDownloader.java
+++ app/src/main/java/io/arsenic/updater/utils/UpdateDownloader.java
@@ -127,4 +127,146 @@ public class UpdateDownloader {
         }
     }
 
+    /**
+     *  Downloads the file from the specified URL.
+     *  Downloads to /sdcard/.arsenicupdater/downloads/ directory.
+     **/
+    @SuppressLint("StaticFieldLeak")
+    private class DownloadTask extends AsyncTask<String, Integer, String> {
+
+        private Context context;
+        private PowerManager.WakeLock mWakeLock;
+
+        DownloadTask(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... sUrl) {
+            InputStream input = null;
+            OutputStream output = null;
+            HttpURLConnection connection = null;
+            try {
+                URL url = new URL(sUrl[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                // expect HTTP 200 OK, so we don't mistakenly save error report
+                // instead of the file
+                if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    return "Server returned HTTP " + connection.getResponseCode()
+                            + " " + connection.getResponseMessage();
+                }
+                // this will be useful to display download percentage
+                // might be -1: server did not report the length
+                int fileLength = connection.getContentLength();
+                // download the file
+                checkDir();
+                input = connection.getInputStream();
+                filename = getExternalStorageDirectory() + getString(R.string.download_location) +
+                        URLUtil.guessFileName(url.toString(), null,
+                                MimeTypeMap.getFileExtensionFromUrl(url.toString()));
+                output = new FileOutputStream(filename);
+                byte data[] = new byte[4096];
+                long total = 0;
+                int count;
+                while ((count = input.read(data)) != -1) {
+                    if (isCancelled()) {
+                        input.close();
+                        return null;
+                    }
+                    total += count;
+                    if (fileLength > 0)
+                        publishProgress((int) (total * 100 / fileLength));
+                    output.write(data, 0, count);
+                }
+            } catch (Exception e) {
+                return e.toString();
+            } finally {
+                try {
+                    if (output != null)
+                        output.close();
+                    if (input != null)
+                        input.close();
+                } catch (IOException ignored) {
+                }
+
+                if (connection != null)
+                    connection.disconnect();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Notification
+            Notification notification = mNotification;
+            notification.setProgress(100, 0, false);
+            notificationManager.notify(1, notification.build());
+            PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                    getClass().getName());
+            mWakeLock.acquire(TIMEOUT);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            // Notification
+            Notification notification = mNotification;
+            notification.setProgress(100, progress[0], false);
+            notificationManager.notify(1, notification.build());
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setMax(100);
+            mProgressDialog.setProgress(progress[0]);
+            super.onProgressUpdate(progress);
+        }
+
+        @Override
+        protected void onCancelled(){
+            setNotification(getString(R.string.download_failed), R.drawable.ic_cancel);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            mWakeLock.release();
+            mProgressDialog.dismiss();
+            if (result != null) {
+                Toast.makeText(mContext, "Download error: " + result, Toast.LENGTH_LONG).show();
+                setNotification(getContext()getString(R.string.download_failed), R.drawable.ic_cancel);
+            }
+            else {
+                Toast.makeText(mContext, getString(R.string.download_complete), Toast.LENGTH_SHORT).show();
+                setNotification(getContext().getString(R.string.download_complete), R.drawable.ic_check);
+                KernelUpdater.flashFile(getContext(), filename);
+            }
+        }
+    }
+
+    public void setNotification(String notification_text, int notification_icon) {
+        Notification notification = mNotification;
+        notification.setContentText(notification_text);
+        notification.setSmallIcon(notification_icon);
+        notification.setProgress(0, 0, false);
+        notificationManager.notify(1, notification.build());
+    }
+
+    /**
+     *  Checks whether the download directory exists.
+     *  Tries to create the directory if it does not exist.
+     **/
+    public void checkDir(){
+        File folder = new File(getExternalStorageDirectory() + getContext().getString(R.string.download_location));
+        boolean success = true;
+        if (!folder.exists()) {
+            success = folder.mkdirs();
+        }
+        if (!success) {
+            downloadTask.cancel(true);
+            mProgressDialog.dismiss();
+            Toast.makeText(getContext(), getString(R.string.create_failed), Toast.LENGTH_SHORT).show();
+            setNotification(getString(R.string.download_failed), R.drawable.ic_cancel);
+        }
+    }
+
 }
